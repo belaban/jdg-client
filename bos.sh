bc87b63bcf2f60dea467cabea8ab0bad67ed274f
Bela Ban <belaban@yahoo.com>
2013-04-19 11:33:31 +0200

    ns

---
+++ bos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Starts a node in the BOS site
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: `basename $0` <node name> [<bind address> <port offset>]"
+  exit $E_BADARGS
+fi
+
+## Change config below for different sites
+SITE=BOS
+CONFIG=bos.xml
+MCAST_ADDR=239.2.2.2
+## Change
+
+
+NODE_NAME=$1
+BIND_ADDR=$2
+PORT_OFFSET=$3
+: ${BIND_ADDR:="127.0.0.1"}
+: ${PORT_OFFSET:="0"}
+
+echo "name is $NODE_NAME"
+echo "bind address is $BIND_ADDR"
+echo "port offset is $PORT_OFFSET"
+
+./standalone.sh -c $CONFIG -Dsite=$SITE -b $BIND_ADDR -Djboss.node.name=$NODE_NAME \
+                -Djboss.socket.binding.port-offset=$PORT_OFFSET -u $MCAST_ADDR
+
---
+++ lon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Starts a node in the LON site
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: `basename $0` <node name> [<bind address> <port offset>]"
+  exit $E_BADARGS
+fi
+
+## Change config below for different sites
+SITE=LON
+CONFIG=lon.xml
+MCAST_ADDR=239.1.1.1
+## Change
+
+
+NODE_NAME=$1
+BIND_ADDR=$2
+PORT_OFFSET=$3
+: ${BIND_ADDR:="127.0.0.1"}
+: ${PORT_OFFSET:="0"}
+
+echo "name is $NODE_NAME"
+echo "bind address is $BIND_ADDR"
+echo "port offset is $PORT_OFFSET"
+
+./standalone.sh -c $CONFIG -Dsite=$SITE -b $BIND_ADDR -Djboss.node.name=$NODE_NAME \
+                -Djboss.socket.binding.port-offset=$PORT_OFFSET -u $MCAST_ADDR
+
